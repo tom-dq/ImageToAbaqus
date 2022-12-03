af964b97aa91eac3c5ab50e2143f8d2ea5ac0ca3
Tom Wilson <tom@getdqd.com>
2022-12-03 22:31:38 +1100

    WIP

--- bin/assign_props.py
+++ bin/assign_props.py
@@ -1,9 +1,36 @@
 import collections
+import enum
+import itertools
+import typing
 
 import parse_inp
 import read_image
 
-def main():
+class AbaInpEnt(enum.Enum):
+    # Key is entity type. Value is prefix of name
+    elset = "grain-"
+    section = "crystal-"
+    material = "metal-"
+
+
+def batched(iterable, n):
+    "Batch data into lists of length n. The last batch may be shorter."
+    # https://docs.python.org/3/library/itertools.html
+    # batched('ABCDEFG', 3) --> ABC DEF G
+    if n < 1:
+        raise ValueError('n must be at least one')
+    it = iter(iterable)
+    while (batch := list(itertools.islice(it, n))):
+        yield batch
+
+
+def _put_in_lines(tokens, n_per_line: int) -> typing.Iterable[str]:
+    """Chunk things up to some number on each line, Abaqus style."""
+    for line_chunk in batched(tokens, n_per_line):
+        yield ", ".join(str(x) for x in line_chunk)
+
+
+def get_grain_labels():
     # Get the labeled image with "grains"
     img = read_image.read_raw_img(read_image.FN)
     _, raw_labeled = read_image.mean_shift(50, 1000, img)
@@ -24,6 +51,7 @@ def main():
         
         prop_to_elem_nums[labeled_point].append(n.num)
 
+    return prop_to_elem_nums
     for grain_num, elems in sorted(prop_to_elem_nums.items()):
         print(grain_num, elems)
 
@@ -31,6 +59,37 @@ def main():
     # *Elset, elset=grain-18  
     # 769,  838,  991, ...   """
 
-if __name__ == "__main__":
-    main()
+
+def get_name(ent_type: AbaInpEnt, label) -> str:
+    return f"{ent_type.value}{label}"
+
+def _one_elset(label, elems):
+    yield f"*Elset, elset={get_name(AbaInpEnt.elset, label)}"
+    yield from _put_in_lines(elems, 16)
+
+def _one_section(label, elems):
+    yield f"** Section: {get_name(AbaInpEnt.section, label)}"
+    yield f"*Solid Section, elset={get_name(AbaInpEnt.elset, label)}, material={get_name(AbaInpEnt.material, label)}"
+
+def make_abaqus_lines(prop_to_elem_nums: typing.Dict[str, typing.List[int]]) -> typing.Iterable[str]:
+    # Element sets
     
+    for maker_func in [_one_elset, _one_section]:
+        for label, elems in prop_to_elem_nums.items():
+            yield from maker_func(label, elems)
+
+
+    
+
+
+    """** Section: crystal-50  
+*Solid Section, elset=grain-50, material=metal-50  
+,  """
+
+
+
+if __name__ == "__main__":
+    prop_to_elem_nums = get_grain_labels()
+    for l in make_abaqus_lines(prop_to_elem_nums):
+        print(l)
+
